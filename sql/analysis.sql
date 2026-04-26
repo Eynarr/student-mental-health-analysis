9c1e20006bd128122633c1b2dcec51db91a947f8
Eynarr <moraleseynar13@gmail.com>
2026-04-25 22:15:34 -0500

    feat: add running average query

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -7,3 +7,17 @@ FROM students
 WHERE student_type = 'International'
 GROUP BY years_country 
 ORDER BY years_country DESC;
+
+-- 2. Running average of depression
+
+WITH depression_per_year AS(
+SELECT years_country AS years,
+	   ROUND(AVG(depression_score)::NUMERIC,2) AS avg_year_depression
+FROM students
+WHERE student_type = 'International'
+GROUP BY years_country)
+SELECT years,
+       avg_year_depression,
+       ROUND(AVG(avg_year_depression) OVER(ORDER BY years ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW),2) AS running_avg
+FROM depression_per_year 
+ORDER BY years;
